f2e84d566acc2168e75e639b97925432800a7e7b
Ilya Tarasov <ilyatarasov75@gmail.com>
2023-01-17 12:26:57 +0300

    change files structure in project

--- src/commands/pdfCreater.js
+++ src/commands/pdfCreater.js
@@ -1,23 +1,23 @@
 import cliCommand from './cliCommand.js';
-import {createPDF} from './pdfService.js';
-import {getDataPaths} from './localFilesGetter.js';
+import { createPDF } from '../services/pdfService.js';
+import { getDataPaths } from '../services/localFilesGetter.js';
 /**
  * decorator for cli
  */
 class PdfCreater extends cliCommand {
-
+    // TODO refactor constructor
+    // TODO break commandNameAndArg into commandName and arg
+    // TODO add verificator for arg and parameter
     /**
      * constructor
      * @param {function} action
      */
     constructor() {
         const action = function (dataPath, cmd) {
-            getDataPaths(dataPath).then(
-                (files) => {
-                    console.log(files);
-                    createPDF(files, cmd.endpath)
-                }
-            )
+            getDataPaths(dataPath).then((files) => {
+                console.log(files);
+                createPDF(files, cmd.endpath);
+            });
             console.log('pdf file created on ' + cmd.endpath);
         };
         super([
--- src/index.js
+++ src/index.js
@@ -1,13 +1,18 @@
 #!/usr/bin/env node
 import CLI from './cli.js';
-import PdfCreater from './pdfCreater.js';
-import * as pdfService from './pdfService.js';
+import PdfCreater from './commands/pdfCreater.js';
+import * as pdfService from './services/pdfService.js';
 
+// TODO work with error processing
+// TODO add menu
+
+// TODO figure out with parameters, that entering, then cli start
 
 const cli = new CLI();
 const pdfCreater = new PdfCreater();
 
 cli.addCommand(pdfCreater);
 
-cli.start();
+// console.log(process.argv)
 
+cli.start();
